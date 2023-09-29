d176adfff09469869d5457f7a8dbb817c46b908e
Anton <xpe6e3@gmail.com>
2023-09-29 12:12:54 +0600

    1

--- js/scripts.js
+++ js/scripts.js
@@ -201,29 +201,69 @@
 
 
     let $owl = $('.carousel-2');
-    $owl.children().each( function( index ) {
-    $(this).attr( 'data-position', index );
-});
-$(document).on('click', '.owl-item>div', function() {
-    let $speed = 300;
-    $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
-});    
-$owl.owlCarousel({
-    loop:true,
-    center: true,
-    stagePadding: 70,
-    nav: true,
-    dots: false,
-    navText: ['&lsaquo;', '&rsaquo;'],
-    responsive:{
-        0:{items:1},
-        600:{items:2},
-        992:{items:3},
-        1200:{items:4},
-        1600:{items:5}            
-    }
-});
+    $owl.children().each(function (index) {
+        $(this).attr('data-position', index);
+    });
+    $(document).on('click', '.owl-item>div', function () {
+        let $speed = 300;
+        $owl.trigger('to.owl.carousel', [$(this).data('position'), $speed]);
+    });
+    $owl.owlCarousel({
+        loop: true,
+        center: true,
+        stagePadding: 70,
+        nav: true,
+        dots: false,
+        navText: ['&lsaquo;', '&rsaquo;'],
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            992: { items: 3 },
+            1200: { items: 4 },
+            1600: { items: 5 }
+        }
+    });
 
+
+
+    // Находим все элементы с классом "modal-trigger" (ссылки на изображения)
+    const modalTriggers = document.querySelectorAll('.modal-trigger');
+    const modal = document.querySelector('.modal-img');
+    const modalImg = document.getElementById('modal-img');
+    const closeModal = document.querySelector('.close-modal');
+
+    // Функция для открытия модального окна
+    function openModal(e) {
+        e.preventDefault(); // Предотвращаем переход по ссылке
+        modal.style.display = 'block';
+        modalImg.src = this.querySelector('img').src;
+    }
+
+    // Функция для закрытия модального окна
+    function closeModalFn() {
+        modal.style.display = 'none';
+    }
+
+    // Привязываем события к элементам
+    modalTriggers.forEach(trigger => {
+        trigger.addEventListener('click', openModal);
+    });
+
+    closeModal.addEventListener('click', closeModalFn);
+
+    // Закрывать модальное окно также при клике вне изображения
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModalFn();
+        }
+    });
+
+    // Закрывать модальное окно при нажатии клавиши Escape
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            closeModalFn();
+        }
+    });
 
 
 })(jQuery);
